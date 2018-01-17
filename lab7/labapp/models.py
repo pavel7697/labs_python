14d1577d7949b40c1de93528395cb17b0c9b4e83
pavel7697 <pavel7697@gmail.com>
2018-01-18 01:30:00 +0300

    dz finished v2.3

--- lab7/labapp/models.py
+++ lab7/labapp/models.py
@@ -10,7 +10,7 @@ class Teacher(models.Model):
     phone = models.BigIntegerField()
     description=models.CharField(max_length=1000)
     mail = models.CharField(max_length=30)
-    img = models.FileField(upload_to='images')
+    img = models.FileField(upload_to='images', default='img/def.jpg')
 
 class Pulpit(models.Model):
     name = models.CharField(max_length=5)
